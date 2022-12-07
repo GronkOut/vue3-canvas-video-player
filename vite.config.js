bb8fbf7a312117d9d97438cae47072592336f498
gronkout <gronkout@ncsoft.com>
2022-12-08 08:22:36 +0900

    Vue3 Canvas Video Player

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  publicDir: false,
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.js'),
